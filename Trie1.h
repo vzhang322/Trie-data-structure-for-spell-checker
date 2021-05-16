8c0c8620f06a6173d170412cbe18a536ae81c873
ShiyunTao421 <80436270+ShiyunTao421@users.noreply.github.com>
2021-05-16 15:14:02 +1200

    Add files via upload

---
+++ Trie1.h
@@ -0,0 +1,28 @@
+#ifndef TRIE_SPELLCHECKER_TRIE_H
+#define TRIE_SPELLCHECKER_TRIE_H
+
+#include "TrieLeafNode.h"
+#include "TrieNonLeafNode.h"
+#include <cstring>
+using namespace std;
+
+class Trie
+{
+public:
+    Trie();
+    Trie(char *);
+    void printTrie();
+    void insert(char *);
+    bool wordFound(char *);
+private:
+    TrieNonLeafNode *root;
+    const int notFound;
+    char prefix[80];
+    int position(TrieNonLeafNode *, char);
+    void addCell(char, TrieNonLeafNode *, int);
+    void createLeaf(char, char *, TrieNonLeafNode *);
+    void printTrie(int, TrieNonLeafNode *, char *);
+};
+
+
+#endif //TRIE_SPELLCHECKER_TRIE_H
